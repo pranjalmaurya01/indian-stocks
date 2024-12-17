ed768979669475c87804f6ed78e2023b26080298
Pranjal Maurya <pranjalmaurya01@gmail.com>
2024-12-17 10:04:23 +0530

    fix: send noti on api call

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -29,3 +29,31 @@ self.addEventListener("install", async () => {
     console.error(e);
   }
 });
+
+// Handle notification click event.
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const clickActionUrl = event.notification.data?.click_action
+
+  if (!clickActionUrl)
+    event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+        // If a tab is already open with the URL, focus on it
+        for (let client of clientList) {
+          if ('focus' in client) {
+            return client.focus();
+          }
+        }
+        // If no matching tab, open a new one
+        if (clients.openWindow) {
+          return clients.openWindow('/');
+        }
+      })
+    );
+  else {
+    event.waitUntil(
+      clients.openWindow(clickActionUrl)
+    )
+  }
+});
